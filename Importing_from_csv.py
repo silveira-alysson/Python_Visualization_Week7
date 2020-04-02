e831bbb91b0fb7e80b68c7080c3e07bb6024917d
Alysson Silveira <54606051+silveira-alysson@users.noreply.github.com>
2020-04-02 20:41:44 -0300

    Add files via upload

---
+++ Importing_from_csv.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 15:34:57 2020
+
+@author: alyss
+"""
+import pandas as pd
+location =  r'C:\Users\alyss\Desktop\MS BAIS\Spring 2020\ISM6419_Data_Visualization\Week_7_Python\datasets\datasets\all_040_in_08.P1.csv'
+
+df = pd.read_csv(location, header = 0)
+
---
+++ Importing_from_excel.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 15:33:16 2020
+
+@author: alyss
+"""
+
+import pandas as pd
+location = r'C:\Users\alyss\Desktop\MS BAIS\Spring 2020\ISM6419_Data_Visualization\Week_7_Python\Assignment1\HEA02.xls'
+df = pd.read_excel(location)
+
